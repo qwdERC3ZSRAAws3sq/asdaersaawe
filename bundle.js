d052a170f4cd3ab211ac29610edbd099dd0e4fff
qwdERC3ZSRAAws3sq <42449125+qwdERC3ZSRAAws3sq@users.noreply.github.com>
2018-08-17 21:00:47 -0400

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -5641,7 +5641,8 @@
     }
 
     function Ln(e, t, n) {
-        void 0 != e && (O.XP = e), void 0 != t && (O.maxXP = t), void 0 != n && (O.age = n), n == a.maxAge ? (Le.innerHTML = "How'd you get this far?", He.style.width = "100%") : if(!pLocation)pLocation = undefined,(Le.innerHTML = "AGE " + O.age + pLocation, He.style.width = O.XP / O.maxXP * 100 + "%")
+        if(!pLocation)pLocation = undefined;
+        void 0 != e && (O.XP = e), void 0 != t && (O.maxXP = t), void 0 != n && (O.age = n), n == a.maxAge ? (Le.innerHTML = "How'd you get this far?", He.style.width = "100%") : (Le.innerHTML = "AGE " + O.age + pLocation, He.style.width = O.XP / O.maxXP * 100 + "%")
     }
 
     function Hn(e) {
